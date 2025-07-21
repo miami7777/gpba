42cef051052e8e2bc05d23e96e03f83720fd63f4
miami7777 <chess_show@mail.ru>
2025-07-21 23:12:53 +0300

    Добавил в seed 15 офферов

--- testGPBA/Domain/AppDbContext.cs
+++ testGPBA/Domain/AppDbContext.cs
@@ -27,7 +27,17 @@ namespace testGPBA.Domain
                 new Offer { Id = 1, Brand = "Toyota", Model = "Camry", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-5) },
                 new Offer { Id = 2, Brand = "Honda", Model = "Accord", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-4) },
                 new Offer { Id = 3, Brand = "Ford", Model = "Focus", SupplierId = 2, RegistrationDate = DateTime.Now.AddDays(-3) },
-                
+                new Offer { Id = 4, Brand = "Toyota", Model = "Camry", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-5) },
+                new Offer { Id = 5, Brand = "Honda", Model = "Accord", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-4) },
+                new Offer { Id = 6, Brand = "Ford", Model = "Focus", SupplierId = 2, RegistrationDate = DateTime.Now.AddDays(-3) },
+                new Offer { Id = 7, Brand = "Toyota", Model = "Camry", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-5) },
+                new Offer { Id = 8, Brand = "Honda", Model = "Accord", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-4) },
+                new Offer { Id = 9, Brand = "Ford", Model = "Focus", SupplierId = 4, RegistrationDate = DateTime.Now.AddDays(-3) },
+                new Offer { Id = 10, Brand = "Ford", Model = "Focus", SupplierId = 2, RegistrationDate = DateTime.Now.AddDays(-3) },
+                new Offer { Id = 11, Brand = "Toyota", Model = "Camry", SupplierId = 1, RegistrationDate = DateTime.Now.AddDays(-5) },
+                new Offer { Id = 12, Brand = "Honda", Model = "Accord", SupplierId = 3, RegistrationDate = DateTime.Now.AddDays(-4) },
+                new Offer { Id = 13, Brand = "Ford", Model = "Focus", SupplierId = 2, RegistrationDate = DateTime.Now.AddDays(-3) },
+                new Offer { Id = 14, Brand = "Ford", Model = "Focus", SupplierId = 2, RegistrationDate = DateTime.Now.AddDays(-3) },
                 new Offer { Id = 15, Brand = "BMW", Model = "X5", SupplierId = 5, RegistrationDate = DateTime.Now }
             );
         }
