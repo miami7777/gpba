0dab22ab2783d079e687dd5320fc75f989b73747
miami7777 <chess_show@mail.ru>
2025-07-29 12:06:12 +0300

    Валидация и обработка ошибок

--- testGPBA/PublicApi/Controllers/OffersController.cs
+++ testGPBA/PublicApi/Controllers/OffersController.cs
@@ -6,6 +6,7 @@ using Core.DTO;
 using AutoMapper;
 using ApplicationCore.Interfaces;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.Extensions.Logging;
 
 namespace testGPBA.Controllers
 {
@@ -14,23 +15,36 @@ namespace testGPBA.Controllers
     public class OffersController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
         private readonly IOfferRepositories _repository;
         private readonly IOffersService _offersService;
 
-        public OffersController(IOfferRepositories repository, IOffersService offersService, IMapper mapper)
+        public OffersController(IOfferRepositories repository, IOffersService offersService, IMapper mapper, ILogger<OffersController> logger)
         {
             _repository = repository;
             _offersService = offersService;
             _mapper = mapper;
+            _logger = logger;
         }
 
         [HttpPost]
         public async Task<ActionResult<OfferDTO>> CreateOffer(AddOfferRequest offerRequest)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var offer = _mapper.Map<Offer>(offerRequest);
-
-            var createdOffer = await _offersService.CreateOfferAsync(offer);
-            return CreatedAtAction(nameof(OfferDTO), new { id = createdOffer.Id }, createdOffer);
+            try
+            {
+                var createdOffer = await _offersService.CreateOfferAsync(offer);
+                return CreatedAtAction(nameof(OfferDTO), new { id = createdOffer.Id }, createdOffer);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Ошибка: {0} \n Подробности: {1}", ex.Message, ex.InnerException);
+                return BadRequest($"Ошибка: {ex.Message}");
+            }
         }
         [HttpGet]
         public async Task<ActionResult> GetAllOffers()
